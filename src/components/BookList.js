5e4920db38063fbd91c63eb1f947cc46d409b833
David Vergaray <david.vergaray.a@gmail.com>
2022-02-22 13:55:31 -0300

    Create BookList component

---
+++ src/components/BookList.js
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux';
+import Book from './Book';
+
+const BookList = () => {
+  const bookStore = useSelector((state) => state.booksReducer);
+
+  return (
+    <div>
+      {
+        bookStore.map((book) => (
+          <Book
+            title={book.title}
+            author={book.author}
+            key={book.id}
+            id={book.id}
+          />
+        ))
+      }
+    </div>
+  );
+};
+
+export default BookList;
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,31 +1,17 @@
 import styled from 'styled-components';
-import { useSelector } from 'react-redux';
-import Book from './Book';
 import BookForm from './BookForm';
+import BookList from './BookList';
 
 const Background = styled.div`
-padding: 1rem 5vw;
-min-height: 100vh;
+  padding: 1rem 5vw;
+  min-height: 100vh;
 `;
 
-const Books = () => {
-  const bookStore = useSelector((state) => state.booksReducer);
-
-  const bookList = bookStore.map((book) => (
-    <Book
-      title={book.title}
-      author={book.author}
-      key={book.id}
-      id={book.id}
-    />
-  ));
-
-  return (
-    <Background>
-      {bookList}
-      {BookForm()}
-    </Background>
-  );
-};
+const Books = () => (
+  <Background>
+    <BookList />
+    <BookForm />
+  </Background>
+);
 
 export default Books;
