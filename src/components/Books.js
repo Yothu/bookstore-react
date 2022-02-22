627042c22e4c78ab46e8338dd13b8abd3a614fc9
David Vergaray <david.vergaray.a@gmail.com>
2022-02-22 13:26:42 -0300

    Add books display

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,59 +1,30 @@
-// import { Provider } from 'react-redux';
 import styled from 'styled-components';
-import { Provider } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import BookForm from './BookForm';
-import store from '../redux/configureStore';
-
-const booksData = [
-  {
-    id: 1,
-    title: 'Rambo',
-    author: 'Popeye',
-  },
-  {
-    id: 2,
-    title: 'Quack',
-    author: 'Mack',
-  },
-  {
-    id: 3,
-    title: 'The Jump',
-    author: 'Siluet',
-  },
-];
 
 const Books = () => {
+  const bookStore = useSelector((state) => state.booksReducer);
+
   const Background = styled.div`
     background-color: lightblue;
     padding: 1rem 5vw;
     min-height: 100vh;
   `;
 
-  // const bookList = booksData.map((book) => (
-  //   <Book
-  //     title={book.title}
-  //     author={book.author}
-  //     key={book.id}
-  //     id={book.id}
-  //   />
-  // ));
+  const bookList = bookStore.map((book) => (
+    <Book
+      title={book.title}
+      author={book.author}
+      key={book.id}
+      id={book.id}
+    />
+  ));
 
   return (
     <Background>
-      {
-        booksData.map((book) => (
-          <Book
-            title={book.title}
-            author={book.author}
-            key={book.id}
-            id={book.id}
-          />
-        ))
-      }
-      <Provider store={store}>
-        <BookForm />
-      </Provider>
+      {bookList}
+      <BookForm />
     </Background>
   );
 };
